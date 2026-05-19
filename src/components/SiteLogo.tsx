cf9d186920232638efe875dad7aacf52598ffb4e
Swapnil Tripathi <64040305+Swap03pathi@users.noreply.github.com>
2026-05-19 19:32:20 +0530

    Added logo and favicon images We are done with the development now. Only content changes required.

--- src/components/SiteLogo.tsx
+++ src/components/SiteLogo.tsx
@@ -3,7 +3,7 @@ import { resolveAssetUrl } from '../utils/assetUrl';
 export default function SiteLogo({ className = 'h-8 w-8' }: { className?: string }) {
   return (
     <img
-      src={resolveAssetUrl('/logo.svg')}
+      src={resolveAssetUrl('/favicon.svg')}
       alt=""
       className={`shrink-0 rounded-md object-contain transition-transform duration-200 group-hover:scale-[1.03] ${className}`}
       width={32}
